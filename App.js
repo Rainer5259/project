b311ed753aa3e7b3afaa6809c5a973e902def23c
Rainer Cordeiro <rainer_tribosk8@hotmail.com>
2020-12-17 22:58:33 -0300

    Projects

---
+++ App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hi, i opened!</Text>
+      <Text>Hi, too!</Text>
+      <Text>Again</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#bbb',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
